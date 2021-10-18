75bc66268277b45d9274b1327cdc7dda46a54a68
Markus Rossio <argitavis@gmail.com>
2021-10-18 16:12:27 -0500

    Add inital file set up for activity testing

---
+++ test/Activity-test.js
@@ -0,0 +1,11 @@
+import { assert } from 'chai';
+import { userData, hydrationData, sleepData, activityData } from '../src/data/sampleData';
+import Activity from '../src/Activity';
+
+describe('Activity', function() {
+  let activity;
+
+  beforeEach(function() {
+    activity = new Activity();
+  });
+});

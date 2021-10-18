ca5c85484a63c99a8aaf8c984299e7df79a63f88
Michele Comfort <michelecomfort@gmail.com>
2021-10-18 14:36:39 -0600

    Add methods to User class

---
+++ src/User.js
@@ -0,0 +1,35 @@
+export default class User {
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.address = null;
+    this.email = null;
+    this.strideLength = null;
+    this.dailyStepGoal = null;
+    this.friends = null;
+    this.hydrationData = null
+    this.sleepData = null
+    this.activityData = null
+  }
+
+  initializeUser(userData, ) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
+
+    this.sleepData = new Sleep();
+    this.activityData = new Activity();
+  }
+
+  retrieveHydrationData(id) {
+    this.hydrationData = new Hydration();
+    
+  }
+
+
+
+}

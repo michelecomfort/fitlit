0711fdb3fb23c8838265b2c8fc2ef9d4e3c27fef
Markus Rossio <argitavis@gmail.com>
2021-11-02 16:52:01 -0500

    Add test to activity to get user miles walked on a given day

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -17,28 +17,28 @@ describe('Activity', function() {
       },
       {
       "userID": 1,
-      "date": "2019/06/15",
+      "date": "2019/07/15",
       "numSteps": 4294,
       "minutesActive": 138,
       "flightsOfStairs": 10
       },
       {
       "userID": 1,
-      "date": "2019/06/15",
+      "date": "2019/08/15",
       "numSteps": 7402,
       "minutesActive": 116,
       "flightsOfStairs": 33
       },
       {
       "userID": 1,
-      "date": "2019/06/15",
+      "date": "2019/09/15",
       "numSteps": 3486,
       "minutesActive": 114,
       "flightsOfStairs": 32
       },
       {
       "userID": 1,
-      "date": "2019/06/15",
+      "date": "2019/10/15",
       "numSteps": 11374,
       "minutesActive": 213,
       "flightsOfStairs": 13
@@ -64,6 +64,6 @@ describe('Activity', function() {
   });
 
   it('should return the miles a user has walked on a given date', function() {
-
+    assert.equal(activity.milesWalked('2019/06/15'), 2.9);
   });
 });

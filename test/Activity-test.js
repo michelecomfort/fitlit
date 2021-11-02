5f16a80aa694c914bd81ecf5e6e0b065119683e8
Markus Rossio <argitavis@gmail.com>
2021-11-02 14:48:40 -0500

    Add functionality test to activity class checking data is getting set properly

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -4,9 +4,45 @@ import Activity from '../src/Activity';
 
 describe('Activity', function() {
   let activity;
+  let userActivityData;
 
   beforeEach(function() {
     activity = new Activity();
+    userActivityData = [{
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 4294,
+      "minutesActive": 138,
+      "flightsOfStairs": 10
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 7402,
+      "minutesActive": 116,
+      "flightsOfStairs": 33
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3486,
+      "minutesActive": 114,
+      "flightsOfStairs": 32
+      },
+      {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 11374,
+      "minutesActive": 213,
+      "flightsOfStairs": 13
+      }];
   });
 
   it('should be a function', function() {
@@ -15,5 +51,13 @@ describe('Activity', function() {
 
   it('should be an instance of activity', function() {
     assert.instanceOf(activity, Activity);
+  });
+
+  it('should be instantiated with activity data', function() {
+    assert.deepEqual(activity.activityData, userActivityData);
+  });
+
+  it('should return the miles a user has walked on a given date', function() {
+
   });
 });

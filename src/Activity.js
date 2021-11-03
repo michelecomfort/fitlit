2c787025c99fb9efe76c0e3c6394b37b449964ac
Markus Rossio <argitavis@gmail.com>
2021-11-02 20:19:04 -0500

    Update activityData name to userActivityData in all activity methods

--- src/Activity.js
+++ src/Activity.js
@@ -6,25 +6,25 @@ export default class Activity {
   }
 
   milesWalked(date) {
-    const day = this.activityData.find(user => user.date === date);
+    const day = this.userActivityData.find(user => user.date === date);
     const feetWalked = this.userStrideLength * day.numSteps;
     const milesWalked = feetWalked / 5280;
     return milesWalked.toFixed(1);
   }
 
   activeMinutes(date) {
-    const day = this.activityData.find(data => data.date === date);
+    const day = this.userActivityData.find(data => data.date === date);
     return day.minutesActive;
   }
 
   averageActiveMinutes(startDate) {
     let startIndex;
-    this.activityData.find((data, index) => {
+    this.userActivityData.find((data, index) => {
       if (data.date === startDate) {
         startIndex = index;
       }
     });
-    const week = this.activityData.reduce((week, data, index) => {
+    const week = this.userActivityData.reduce((week, data, index) => {
       if (index >= startIndex && index < startIndex + 6) {
         week.push(data);
       }
@@ -38,7 +38,7 @@ export default class Activity {
   }
 
   checkStepGoal(date) {
-    const day = this.activityData.find(data => data.date === date);
+    const day = this.userActivityData.find(data => data.date === date);
     if (day.numSteps >= this.userDailyStepGoal) {
       return true;
     } else {

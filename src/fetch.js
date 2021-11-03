946a2a321f47246ca60a03994bc777536c1bcb27
Markus Rossio <argitavis@gmail.com>
2021-11-03 17:23:04 -0500

    Remove unneeded then in fetch file

--- src/fetch.js
+++ src/fetch.js
@@ -1,33 +1,21 @@
 const getUserData = () => {
   return fetch('http://localhost:3001/api/v1/users')
-    .then(result => result.json())
-    .then(data => {
-      return data
-    });
+    .then(result => result.json());
 };
 
 const getSleepData = () => {
   return fetch('http://localhost:3001/api/v1/sleep')
-    .then(result => result.json())
-    .then(data => {
-      return data
-    });
+    .then(result => result.json());
 };
 
 const getActivityData = () => {
   return fetch('http://localhost:3001/api/v1/activity')
-    .then(result => result.json())
-    .then(data => {
-      return data
-    });
+    .then(result => result.json());
 };
 
 const getHydrationData = () => {
   return fetch('http://localhost:3001/api/v1/hydration')
-    .then(result => result.json())
-    .then(data => {
-      return data
-    });
+    .then(result => result.json());
 };
 
 export {
